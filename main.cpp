1e6632708631358c1b669f53ebb64112b88986b8
Sylwia Juda <s25373@szuflandia.pjwstk.edu.pl>
2021-11-06 14:27:40 +0100

    pierwsze licenie

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+int a,b,c;
+std::cout<<"1 number: "<<std::endl;
+std::cin>>a;
+std::cout<<"2 number: "<<std::endl;
+std::cin>>b;
+std::cout<<"3 number: "<<std::endl;
+std::cin>>c;
+std::cout<<"your new number: "<< (a+b+c)/3<<std::endl;
+return 0;
+}
